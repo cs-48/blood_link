d20b5e9975909328d3b454e16993fc196fff66fe
cs-48 <85981551+cs-48@users.noreply.github.com>
2024-02-13 15:21:23 +0530

    if true for home page

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -13,14 +13,14 @@ class AuthPage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           //user is logged in
-          if (snapshot.hasData) {
+          if (true) {  //snapshot.hasData
             return HomePage();
           }
 
           //user is NOT logged in
-          else {
-            return LoginOrRegisterPage();
-          }
+          // else {
+          //   return LoginOrRegisterPage();
+          // }
         },
       )
     );
