5d7eff39e25d8ddcef372be4a68786454d1796a0
cs-48 <85981551+cs-48@users.noreply.github.com>
2024-04-07 20:59:55 +0530

    first commit for chat

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,195 @@
+import 'package:blood_link/components/for_chat/chat_bubble.dart';
+import 'package:blood_link/components/for_chat/my_textfield_for_chat.dart';
+import 'package:blood_link/services/chat/auth_service_for_chat.dart';
+import 'package:blood_link/services/chat/chat_service.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class ChatPage extends StatefulWidget {
+  final String recieverEmail;
+  final String recieverID;
+
+  const ChatPage({super.key, required this.recieverEmail, required this.recieverID});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  //text controller
+  final TextEditingController _messageController = TextEditingController();
+
+  //chat & auth services
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
+
+  //for textfield focus
+  FocusNode myFocusNode = FocusNode();
+
+  @override
+  void iniState() {
+    super.initState();
+
+    //add listener to focus node
+    myFocusNode.addListener(() {
+      if (myFocusNode.hasFocus) {
+        //cause a delay so that the keyboard has time to show up
+        //then the amount of remaining space will be calculated
+        //then scroll down
+        Future.delayed(
+          const Duration(milliseconds: 500),
+          () => scrollDown(),
+        );
+      }
+    });
+
+    //wait a bit for listview to be built, the scroll to bottom
+    Future.delayed(
+      const Duration(milliseconds: 500),
+      () => scrollDown(),
+    );
+  }
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  //scroll controller
+  final ScrollController _scrollController = ScrollController();
+  void scrollDown() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(seconds: 1),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  //send messages
+  void sendMessage() async {
+    //if there is something inside the textfield
+    if (_messageController.text.isNotEmpty) {
+      //send the message
+      await _chatService.sendMessage(
+          widget.recieverID, _messageController.text);
+
+      //clear text controller
+      _messageController.clear();
+    }
+
+    scrollDown();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.grey,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 47.0),
+          child: Text(widget.recieverEmail),
+        ),
+      ),
+      body: Column(
+        children: [
+          //display all messages
+          Expanded(
+            child: _buildMessageList(),
+          ),
+
+          //user input
+          _buildUserInput(),
+        ],
+      ),
+    );
+  }
+
+  //build message list
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+      stream: _chatService.getMessages(widget.recieverID, senderID),
+      builder: (context, snapshot) {
+        //errors
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        //loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading...");
+        }
+
+        //return list view
+        return ListView(
+          controller: _scrollController,
+          children:
+              snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
+        );
+      },
+    );
+  }
+
+  //build message item
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    //is current user
+    bool isCurrentUser = data['senderID'] == _authService.getCurrentUser()!.uid;
+
+    //align message to the right if sender is the current user, otherwise left
+    var alignment =
+        isCurrentUser ? Alignment.centerRight : Alignment.centerLeft;
+
+    return Container(
+      alignment: alignment,
+      child: Column(
+        crossAxisAlignment:
+            isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          ChatBubble(
+            message: data["message"],
+            isCurrentUser: isCurrentUser,
+          )
+        ],
+      ),
+    );
+  }
+
+  //build message input
+  Widget _buildUserInput() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50.0),
+      child: Row(
+        children: [
+          //textfield should take ip most of the space
+          Expanded(
+            child: MyTextFieldForChat(
+              controller: _messageController,
+              hintText: "Type a message",
+              obsecureText: false,
+              focusNode: myFocusNode,
+            ),
+          ),
+
+          //send button
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.green,
+              shape: BoxShape.circle,
+            ),
+            margin: const EdgeInsets.only(right: 25),
+            child: IconButton(
+              onPressed: sendMessage,
+              icon: const Icon(Icons.arrow_upward),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
